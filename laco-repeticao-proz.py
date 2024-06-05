1c289d39c8320b21d78972ea5a59724204f235b0
dailaDanielle <166344173+dailaDanielle@users.noreply.github.com>
2024-06-05 18:50:59 -0300

    Add files via upload

---
+++ laco-repeticao-proz.py
@@ -0,0 +1,17 @@
+# com while crescente
+andar = 0
+while andar <= 20:
+    if andar != 13:
+        print(andar)
+    andar += 1
+# com for crescente
+for andar1 in range(0, 20, 1):
+    if andar1 != 13:
+        print(andar1)
+# com while decrescente
+andar = 20
+while andar > 0:
+    if andar != 13:
+        print(andar)
+    andar -= 1
+
